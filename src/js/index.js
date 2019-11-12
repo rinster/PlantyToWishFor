107f61bee0a23fd4646ab2d9c3c2c587ca06ded6
Erine Natnat <enatnat.08@gmail.com>
2019-11-11 23:00:32 -0800

    Font awesome import test success

--- src/js/index.js
+++ src/js/index.js
@@ -6,6 +6,12 @@ import '../styles/scss/main.scss';
 
 import PMelano from '../assets/images/PMelano.jpg'; //image load test
 
+// FONT AWESOME IMPORT ===============================
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
+import '@fortawesome/fontawesome-free/js/regular'
+import '@fortawesome/fontawesome-free/js/brands'
+
 
 let header = new Header();
 let firstHeading = header.getFirstHeading();
