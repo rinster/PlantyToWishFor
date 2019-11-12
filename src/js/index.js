748e51bbb09f17a6ee33cb2fed76d3fa94533ef0
Erine Natnat <enatnat.08@gmail.com>
2019-11-11 22:53:33 -0800

    code comments

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,16 @@
+// CLASS IMPORT =======================================
 import { Header } from './models/header';
 
-import '../styles/scss/main.scss'; //IMPORT sass
+// STYLING & ASSET IMPORT =============================
+import '../styles/scss/main.scss'; 
 
 import PMelano from '../assets/images/PMelano.jpg'; //image load test
+
 
 let header = new Header();
 let firstHeading = header.getFirstHeading();
 console.log(firstHeading);
+
 
 //Image import test
 document.getElementById('PhiloMelano').setAttribute('src',PMelano)
